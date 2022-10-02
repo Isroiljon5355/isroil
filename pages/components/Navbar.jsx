834bee0db56ac7caccd9aace57c5b7ac2bef0e33
Isroiljon5355 <isroiljon5355@gmail.com>
2022-10-02 22:46:19 +0500

    Navbar qo'shdim

---
+++ pages/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Navbar() {
+	return (
+		<div>
+			<nav>
+				<ul>
+					<li>
+						<a href="#">a</a>
+					</li>
+					<li>
+						<a href="#">a</a>
+					</li>
+					<li>
+						<a href="#">a</a>
+					</li>
+					<li>
+						<a href="#">a</a>
+					</li>
+				</ul>
+			</nav>
+		</div>
+	);
+}
+
+export default Navbar;
