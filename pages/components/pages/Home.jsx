cd195d1175f0e4a9ac18754129ac900b846ac4dc
Isroiljon5355 <isroiljon5355@gmail.com>
2022-10-06 22:40:08 +0500

    text add

--- pages/components/pages/Home.jsx
+++ pages/components/pages/Home.jsx
@@ -12,6 +12,7 @@ function Home() {
 				alignContent: "center",
 			}}
 		>
+			
 			<Box>
 				<Image
 					src="https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
@@ -44,6 +45,10 @@ function Home() {
 				>
 					I am a frontend developer
 				</Text>
+
+
+
+				
 				<Icon />
 			</Box>
 		</Box>
