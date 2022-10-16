1912c21685734e43b425d8a6229910c2f3014e17
Isroiljon5355 <isroiljon5355@gmail.com>
2022-10-16 23:39:13 +0500

    navbar add

--- src/components/navbar/NavbarLinks.jsx
+++ src/components/navbar/NavbarLinks.jsx
@@ -1,35 +1,68 @@
-import React from "react"
-import Link from "next/link"
-import { Badge, Box, NavLink } from "@mantine/core"
-import {
-  IconHome2,
-  IconGauge,
-  IconChevronRight,
-  IconActivity,
-  IconCircleOff,
-} from "@tabler/icons"
+import React from "react";
+import Link from "next/link";
+import { Badge, Box, NavLink, Text } from "@mantine/core";
+import { IconHome2 } from "@tabler/icons";
 function NavbarLinks() {
   return (
     <>
-      <Link
-        href="/"
+      <Box
         style={{
-          textDecoration: "none !important",
-          color: "#fff",
+          display: "flex",
+          flexWrap: "wrap",
+          alignItems: "center",
+          alignContent: "space-between",
+          // border: "1px solid red",
+          height: "85vh",
+          padding: "5%",
         }}
       >
-        <NavLink
-          label="With icon"
-          variant="filled"
-          active
-          icon={<IconHome2 size={16} stroke={1.5} />}
-        />
-      </Link>
-      <Link href="/about">
-        <NavLink label="About" icon={<IconHome2 size={16} stroke={1.5} />} />
-      </Link>
+        <Box
+          style={{
+            width: "100%",
+          }}
+        >
+          <Link href="/">
+            <Text weight={700} style={{ fontSize: "35px", cursor: "pointer" }}>
+              Isroiljon
+            </Text>
+          </Link>
+        </Box>
+        <Box
+          style={{
+            width: "100%",
+          }}
+        >
+          <Link
+            href="/"
+            style={{
+              textDecoration: "none !important",
+              color: "#fff",
+            }}
+          >
+            <NavLink
+              label="With icon"
+              variant="filled"
+              active
+              icon={<IconHome2 size={16} stroke={1.5} />}
+            />
+          </Link>
+          <Link href="/about">
+            <NavLink
+              label="About"
+              icon={<IconHome2 size={16} stroke={1.5} />}
+            />
+          </Link>
+        </Box>
+        <Box
+          style={{
+            width: "100%",
+          }}
+        >
+          ;mkds
+        </Box>
+      </Box>
     </>
-  )
+  );
 }
 
-export default NavbarLinks
+export default NavbarLinks;
