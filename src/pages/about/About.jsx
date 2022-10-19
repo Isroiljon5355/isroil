db598b9de19f72c90b39aa9674c0dad14f5dafb1
Isroiljon5355 <isroiljon5355@gmail.com>
2022-10-19 22:42:00 +0500

    add about

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,6 +1,14 @@
-import React from "react"
+import { Box, Text } from "@mantine/core";
+import React from "react";
 function About() {
-  return <div>about</div>
+  return (
+    <div>
+      <Text>About me</Text>
+      <Box>
+        <Text>Musajonov Isroiljon Frontend Developer</Text>
+      </Box>
+    </div>
+  );
 }
 
-export default About
+export default About;
