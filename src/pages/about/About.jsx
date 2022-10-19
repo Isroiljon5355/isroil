dc2975fa17fa7d41d5ba6bc711d34eed91b508da
Isroiljon5355 <isroiljon5355@gmail.com>
2022-10-19 22:45:57 +0500

    add text

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -7,6 +7,13 @@ function About() {
       <Box>
         <Text>Musajonov Isroiljon Frontend Developer</Text>
       </Box>
+      <Box>
+        <Text>
+          Hi there. My name is Israel. I am a frontend developer. I have been
+          doing programming for over 2 years and I have been doing front end for
+          1.5 years. I can create various HTML layouts and ReactJS SPA.
+        </Text>
+      </Box>
     </div>
   );
 }
